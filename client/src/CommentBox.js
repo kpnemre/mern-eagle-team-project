35594666ae7d19147db72c771ebf1fd0a9aaf5eb
kpnemre <kpnemre51@gmail.com>
2020-12-15 00:49:13 +0300

    Init comment blocks. Add aimed page photo

---
+++ client/src/CommentBox.js
@@ -0,0 +1,22 @@
+
+//CommentBox is main file and we export to index.js 
+
+function CommentBox() {
+  return (
+    <div className="App">
+    <div className="App">
+<h1>Comments:</h1>
+
+{/* 
+TODO: ADD COMMENTS from comment list
+
+TODO: ADD SEND COMMENTS FORM-- Comment Form
+*/}
+      
+    </div>
+    </div>
+
+  );
+}
+
+export default CommentBox;
